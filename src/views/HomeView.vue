6bb887d5c46c50a815c3911b65a4bc07dcc90b43
cfayac <corentin.fayac@orange.fr>
2023-03-21 09:41:57 +0100

    VueJS base

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+// let svg = document.getElementsByTagName("svg")[0];
+// let bbox = svg.getBBox();
+// let viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+// svg.setAttribute("viewBox", viewBox);
+// navigator.clipboard.writeText(svg.outerHTML);
+</script>
+
+<template>
+    <main>
+        <TheWelcome/>
+    </main>
+</template>
